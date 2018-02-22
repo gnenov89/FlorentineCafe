5d6c81b57b28ba3bd328379c8d83298f2999d90e
gnenov89 <georgi.nenov89@gmail.com>
2018-02-22 15:05:19 -0500

    about page sketched

--- js/init.js
+++ js/init.js
@@ -3,6 +3,10 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('h1').fadeIn(4000);
+    $('h5').fadeIn(4000);
+    $('h6').fadeIn(4000);
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
