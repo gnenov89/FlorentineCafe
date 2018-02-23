64350afab5b39e69383f928291b5ab9b9ebc207a
gnenov89 <georgi.nenov89@gmail.com>
2018-02-23 11:26:54 -0500

    about page almost ready

--- js/candy.js
+++ js/candy.js
@@ -1,13 +1,37 @@
 
 
   function initMap(){
+
+    // Map options
     var options = {
-      zoom:8,
-      center:{lat:42.3601,lng:-71.0589}
+      zoom:14,
+      center:{lat:42.3643,lng:-71.0538}
     }
-
+    // New map
     var map = new 
-    google.maps.Map(document.getElementById('mapO'), options);
+    google.maps.Map(document.getElementById('map'), options);
 
+    // New Marker
+     var marker = new google.maps.Marker({
+         position:{lat:42.3643,lng: -71.0538},
+         map:map
+         
+     });
+
+    //  var infoWindow = new google.maps.infoWindow({
+    //      content:'<h1>333 Hanover Str.|Boston, MA, 02113'
+    //  });
+     var infoWindow = new google.maps.InfoWindow({
+        content:'<h2>333 Hanover Str.|Boston, MA, 02113</h2>'
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+      
+
+     marker.addListener('click', function (){
+         infoWindow.open(map, marker);
+     });
   }
   
