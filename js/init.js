910691fe587de3d283b0aa63a0766ac8349bdd84
gnenov89 <georgi.nenov89@gmail.com>
2018-02-26 12:40:14 -0500

    working thru responsive design

--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@
     var list = $('h1, h3, h5, h6, p, img.about').fadeIn(4000);
     $('.carousel').carousel();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
-    
+    $('.materialboxed').materialbox();
 
 
   }); // end of document ready
